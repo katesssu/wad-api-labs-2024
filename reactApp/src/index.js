79082d94595c5239002d460d07fa00f0ff074388
katesssu <katiesugrue18@gmail.com>
2024-05-21 21:44:04 +0100

    Topic 12 - Sign up Page

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -7,7 +7,7 @@ import MoviesPage from "./pages/moviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
 import LoginPage from "./pages/loginPage";
 import AuthContextProvider from "./contexts/authContext";
-import LoginPagege from "./pages/loginPage";
+import SignUpPage from "./pages/signUpPage";
 import ProtectedRoutes from "./protectedRoutes";
 import Header from "./components/siteHeader";
 
@@ -41,6 +41,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<PublicPage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={ <SignUpPage /> } />
             <Route element={<ProtectedRoutes />}>
               <Route path="/movies" element={<MoviesPage />} />
               <Route path="/profile" element={<ProfilePage />} />
